cd360c450939250994ce24cb77ef4d288a10ae06
Gabriel Sampaio <bielsampaio05@hotmail.com>
2024-05-07 07:07:35 -0300

    create: tec502-redes-pbl1

---
+++ Broker/Broker.py
@@ -0,0 +1,380 @@
+import socket
+import threading
+import time
+import queue
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask import *
+
+class Broker:
+
+    def __init__(self, host = 'localhost', port_tcp = 8888, port_udp = 9999, flask_port = 5000):
+
+        # Host e portas
+        self.host = host
+        self.port_tcp = port_tcp
+        self.port_udp = port_udp
+        self.flask_port = flask_port
+
+        # Lista de dispositivos
+        self.devices_list = []
+
+        # Inicilizando filas para organizar menssagens
+        self.udp_queue = queue.Queue()
+        self.http_queue = queue.Queue()
+
+        # Iniciando o Flask
+        self.app = Flask(__name__)
+        CORS(self.app)
+        self.setup_flask_routes()
+
+        # Socket TCP
+        self.socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # SOCK_STREAM = TCP / Via que apenas aceita a comunicação
+        self.bind_tcp()
+
+        # Socket UDP
+        self.socket_udp = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)  # SOCK_DGRAM = UDP / Via que apenas aceita a comunicação
+        self.bind_udp()
+
+        self.essencial_threads()
+
+    #Função que tenta fazer uma ligação via tcp:
+    def bind_tcp(self):
+
+        #Tento fazer uma ligação:
+
+        try:
+            self.socket_tcp.bind((self.host, self.port_tcp))
+            self.socket_tcp.listen(5)   #Recebe até 5 conexões
+            return
+
+        except Exception as e:
+
+            return print('Não foi possível iniciar a ligação via tcp.')
+
+
+    # Função que tenta fazer uma ligação via udp:
+    def bind_udp(self):
+
+        try:
+            self.socket_udp.bind((self.host, self.port_udp))
+            return
+
+        except Exception as e:
+            return print('Não foi possível iniciar a ligação via udp.')
+
+
+    # Função que aceita as conexões via tcp:
+    def start_tcp_connection(self):
+
+        #Sempre aceitando novas conexoes:
+
+        while True:
+            client_socket, client_address = self.socket_tcp.accept()
+
+            # Indentifica se a conexão advém de um dispositivo
+            self.identification(client_socket, client_address)
+
+            # Inicia uma thread para recebimento de dados via tcp
+            tcp_thread = threading.Thread(target=self.handle_tcp, args=(client_socket, client_address), daemon=True)
+            tcp_thread.start()
+
+
+    # Inicia uma thread para recebimento de dados via udp:
+    def start_udp_connection(self):
+
+        # O broker continua a ouvir mensagens UDP
+
+        udp_thread = threading.Thread(target=self.handle_udp, args=(), daemon=True)
+        udp_thread.start()
+
+
+    # Função para identificação do cliente conectado:
+    def identification(self, client_socket, client_address):
+
+        print(f"Nova conexão: {client_address}")
+
+        identification_msg = client_socket.recv(2048).decode('utf-8').strip()
+
+        if identification_msg[:6] == "client":
+
+            print(f"{client_address} é um cliente.")
+
+
+        elif identification_msg[:6] == "device":
+
+            print(f"{client_address} é um dispositivo termostato.")
+
+            self.device_register(client_socket, client_address, identification_msg[7:])
+
+        else:
+            print(f"Não consegui identificar a conexão de {client_address}. Mensagem de identificação: {identification_msg}")
+            client_socket.close()
+            return  # Encerra a conexão se a identificação for desconhecida
+
+
+    # Verifica continuamente se o dispositivo ainda está conectado:
+    def device_confirm_conncetion(self):
+
+        while True:
+
+            for disp in self.devices_list:
+
+                try:
+
+                    disp['socket'].send(bytes("CONNECTION CONFIRMATION", 'utf-8')) # Menssagem de verificação para o dispositivo
+
+                except Exception as e:
+
+                    print(f"Conexão encerrada com o dispositivo: {disp['id']} > ({disp['ip']}:{disp['port']})")
+
+                    self.devices_list.remove(disp) # Removo ele da lista de dispositivos
+
+
+            time.sleep(10)  # Verificar a conexão a cada 10 segundos
+
+
+    # Recebe menssagens via udp:
+    def handle_udp(self):
+
+        while True:
+
+            try:
+                data, address = self.socket_udp.recvfrom(2048)
+
+                self.udp_queue.put((data.decode('utf-8'), address))
+
+            except Exception as e:
+
+                print(f"Falha ao receber mensagem via udp.")
+                break
+
+    # Recebe menssagens via tcp:
+    def handle_tcp(self,client_socket, client_address):
+
+
+        while True:
+            try:
+                # Recebe uma mensagem
+                msg = client_socket.recv(2048).decode('utf-8').strip()
+                print(msg)
+
+            except Exception as e:
+                if hasattr(e, 'winerror') and e.winerror == 10054:
+
+                    print("Dispositivo desconectado!")
+                    self.device_delete(self.find_device_pos_by_socket(client_socket))
+
+
+                else:
+                    print("Não foi possível se conectar ao Broker!")
+                break
+
+        # Fecha a conexão
+
+        client_socket.close()
+        print(f"Connection with {client_address} closed.")
+
+
+    # Tratamento/interpretação dos dados enviados pelo dispositivo:
+    def udp_messages_tratament(self):
+
+        while True:
+
+            if not self.udp_queue.empty(): # Se houver algo na fila
+
+                # Dado e o endereço gaurdados na fila
+
+                data, address = self.udp_queue.get()
+
+                # Interpretando o dado
+
+                data_split = data.split('-')
+
+                type = data_split[0]
+                id = int(data_split[1])
+                state = data_split[2]
+                temperature = data_split[3]
+
+                if type == "dispositive_data":
+
+                    for disp in self.devices_list:
+
+                        if disp["ip"] == address[0] and disp['id'] == int(id):
+
+                            disp["state"] = state
+                            disp["temperature"] = temperature
+
+    # Tratamento/interpretação dos comandos enviados pela aplicação:
+    def process_http_command(self):
+
+        while True:
+
+            if not self.http_queue.empty(): # Se houver algo na fila
+
+                # Protocolo com dados do dispositivo e do comando enviado
+                protocol = self.http_queue.get()
+
+                # Interpretando o protocolo
+
+                type = protocol[0]
+                id = int(protocol[1])
+                ip = protocol[2]
+                command = protocol[4]
+                temperature = int(protocol[5])
+
+                # Enviar a mensagem para os dispositivos TCP conectados
+
+                for disp in self.devices_list:
+
+                    if type == "command":
+
+                        if disp["ip"] == ip and disp['id'] == id:
+
+                            msg = f"command-{id}-{command}-{temperature}"
+
+                            disp["socket"].send(msg.encode())
+
+
+
+    # Função para a execução das principais threads:
+    def essencial_threads(self):
+
+
+        # Inicia uma thread para armazenar as mensagens Udp em uma Fila
+        self.udp_thread = threading.Thread(target=self.handle_udp)
+        self.udp_thread.start()
+
+        # Iniciando o servidor Flask em uma thread separada
+        self.start_flask_thread = threading.Thread(target=self.start_flask, daemon=True)
+        self.start_flask_thread.start()
+
+        # Inicia um thread para confirmar conexões do dispositivo
+        self.device_confirm_conn = threading.Thread(target=self.device_confirm_conncetion, daemon=True)
+        self.device_confirm_conn.start()
+
+        # Inicia uma thread para tratar as mensagens UDP
+        self.udp_messages_trat = threading.Thread(target=self.udp_messages_tratament, daemon=True)
+        self.udp_messages_trat.start()
+
+        # Inicia uma thread para tratar os comandos HTTP
+        self.process_http_command = threading.Thread(target=self.process_http_command, daemon=True)
+        self.process_http_command.start()
+
+
+    #### Funções voltadas para a manipulação dos dispositivos
+
+
+    # Função que registra um novo dispositivo
+    def device_register(self, client_socket, client_address, inf):
+        list_info = inf.split("-")
+
+        device = {
+
+            "id": int(list_info[0]),
+            "ip": client_address[0],
+            "port": client_address[1],
+            "socket": client_socket,
+            "state": list_info[1],
+            "temperature": list_info[2]
+        }
+
+        self.devices_list.append(device)
+        print(self.devices_list)
+
+
+    # Função que encontra posição de um dispositivo pelo socket
+    def find_device_pos_by_socket(self, socket):
+
+        for disp in self.devices_list:
+
+           if disp['socket'] == socket:
+               return self.devices_list.index(disp)
+
+
+    # Função que remove um dispositivo pela posição
+    def device_delete(self, pos):
+
+        del self.devices_list[pos]
+        print("Dispositivo removido com sucesso!")
+
+
+
+    ### Função para designar as rotas para a API
+    def setup_flask_routes(self):
+
+
+        # Função que retorna a lista com os dispositivos
+        @self.app.route('/get-devices', methods=['GET'])
+        def get_devices():
+
+            try:
+                dev_list = []
+
+                for device in self.devices_list:
+
+                    dev = {
+
+                        "id": device["id"],
+                        "ip": device["ip"],
+                        "port": device["port"],
+                        "state": device["state"],
+                        "temperature": device["temperature"]
+
+                    }
+
+                    dev_list.append(dev)
+
+                return jsonify({"success": True, "devices": dev_list}), 200
+
+            except Exception as e:
+
+                return jsonify({"success": False, "message": str(e)}), 500
+
+
+        # Função para envio de comandos
+        @self.app.route("/send-command", methods=["POST"])
+
+        def send_command():
+
+            try:
+                data = request.json
+                message = data.get("message")
+
+                if message is None:
+                    return jsonify({"success": False, "message": "Comando não recebido."}), 400
+
+                command_split = message.split("-")
+
+
+                # Coloca a mensagem na fila de mensagens HTTP
+                self.http_queue.put(command_split)
+
+                type= command_split[0]
+                command = command_split[4]
+
+                if type == "command":
+
+                    if command != "on" and command != "off" and command != "newtemp":
+                        return jsonify({"success": False, "message": "Comando inválido"}), 400
+                    else:
+                        return jsonify({"success": True, "message": "Comando enviado com sucesso."}), 200
+
+            except Exception as e:
+
+                return jsonify({"success": False, "message": str(e)}), 500
+
+    #Função para startar o servidor flask
+    def start_flask(self):
+        self.app.run(host='0.0.0.0', port=self.flask_port)
+
+def main():
+
+    broker = Broker()
+    broker.start_tcp_connection()
+    broker.start_udp_connection()
+
+
+if __name__ == '__main__':
+
+    main()
---
+++ Device/Thermostat.py
@@ -0,0 +1,217 @@
+import socket
+import threading
+import time
+
+class Thermostat:
+
+    def __init__(self, id, initial_temperature = 20 , status= 'off', broker_host='localhost',
+                 broker_tcp_port=8888, broker_udp_port=9999):
+
+        # Atributos do Termostato
+
+        self.id = id
+        self.temperature = initial_temperature
+        self.status = status
+
+        # Host e portas (broker)
+        self.broker_host = broker_host
+        self.broker_tcp_port = broker_tcp_port
+        self.broker_udp_port = broker_udp_port
+
+        # Conexão com o broker TCP E UDP
+        self.connect_to_broker_tcp()
+        self.conncet_to_broker_udp()
+
+        # Inicia a função para ficar enviado os status via UDP
+        self.send_data_via_udp()
+
+        # Inicia a thread para receber as menssagens via tcp
+        self.receive_thread = threading.Thread(target=self.recv_tcp)
+        self.receive_thread.start()
+
+
+    # Função para se conectar com o broker via tcp:
+    def connect_to_broker_tcp(self):
+        while True:
+            try:
+
+                # Tentativa de se conectar ao servidor tcp
+
+                self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Socket tcp
+                self.tcp_socket.connect((self.broker_host, self.broker_tcp_port))
+                self.send_identification()
+
+                print("Conectado!")
+
+                break
+
+            except socket.error as e:
+
+                if hasattr(e, 'winerror') and e.winerror == 10061:
+                    print("O Broker não está respondendo. Verifique se ele está funcionando.")
+                    time.sleep(2)
+                    print("Aguarde a conexão...")
+
+                else:
+                    print(f"Falha ao tentar se conectar com o Broker: {e}")
+
+    # Função para se conectar com o broker via udp:
+    def conncet_to_broker_udp(self):
+
+
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Socket UDP
+
+
+    # Função para receber dados via tcp (comandos da aplicação via broker):
+    def recv_tcp(self):
+
+        while True:
+            try:
+                data = self.tcp_socket.recv(2048).decode('utf-8')
+
+                if not data:
+                    break
+
+                print(f"TCP DATA: {data}")
+
+                #Tratamento do dado:
+
+                self.message_tratament(data)
+
+            except ConnectionResetError:
+                print("A conexão com o Broker foi perdida! Verifique se ele está funcionando.")
+                self.connect_to_broker_tcp()
+
+            except Exception as e:
+                print(f"Falha no recebimento dos dados via TCP.{e}")
+                time.sleep(2)
+
+
+    # Função para enviar a identificação do dispositivo para o broker:
+    def send_identification(self):
+
+        try:
+
+            self.tcp_socket.send(f'device-{self.get_id()}-{self.get_status()}-{self.get_temperature()}'.encode())
+
+        except:
+            return print("Não foi possível se conectar ao Broker!")
+
+
+    # Função para enviar dados via udp:
+    def send_udp(self, message):
+
+        try:
+
+            # Tenta enviar mensagem ao servidor via UDP
+            self.udp_socket.sendto(message.encode(), (self.broker_host, self.broker_udp_port))
+
+        except socket.error as e:
+
+            print(f"Falha ao tentar enviar mensagem via UDP.")
+
+
+    # Função para enviar os dados do dispositivo via udp:
+    def send_data_via_udp(self):
+
+        def enviar():
+            while True:
+                try:
+
+                    actual_data = f"dispositive_data-{self.get_id()}-{self.get_status()}-{self.get_temperature()}"
+
+                    self.send_udp(actual_data)
+                    time.sleep(2)
+
+                except Exception as e:
+
+                    print(f"Falha ao tentar enviar mensagem via UDP.")
+
+        #Thread para ficar enviando os dados do dispositivo de maneira continua
+        thread = threading.Thread(target=enviar)
+        thread.daemon = True
+        thread.start()
+
+
+    # Função para tratar os comandos enviados:
+    def message_tratament(self, message):
+
+        message_split = message.split('-')
+
+        type = message_split[0]
+        command = message_split[2]
+        new_temp = message_split[3]
+
+
+        if type == 'command':
+
+
+            if command == 'on':
+                print("OIIIIIIIIIII")
+                if(self.get_status() == 'off'):
+                    self.set_status()
+
+                    self.tcp_socket.send(bytes(f"accept-command-on-{self.get_status()}", "utf-8"))
+
+            elif command == 'off':
+
+                if (self.get_status() == 'on'):
+                    self.set_status()
+
+                    self.tcp_socket.send(bytes(f"accept-command-off-{self.get_status()}", "utf-8"))
+
+
+            elif command == 'newtemp':
+
+
+                if new_temp:
+
+                    try:
+                        new_temp = int(new_temp)
+
+                        self.set_temperature(new_temp)
+
+                        print(f"Nova temperatura: {self.get_temperature()}°C.")
+
+                        self.tcp_socket.send(bytes(f"accept-command-newtemp-{self.get_temperature()}", "utf-8"))
+
+                    except ValueError:
+                        print("Valor para Temperatura inválido.")
+
+            else:
+                print("Comando inválido")
+
+
+    # Função que retorna o id:
+    def get_id(self):
+
+        return self.id
+
+    # Função que retorna a temperatura:
+    def get_temperature(self):
+        return self.temperature
+
+    # Função que retorna se o termostato está ligado ou desligado:
+    def get_status(self):
+        return self.status
+
+    # Função que altera o valor da temperatura:
+    def set_temperature(self, target_temperature):
+
+        self.temperature = target_temperature
+
+    # Função que altera o estado (ligado ou desligado) do termostato:
+    def set_status(self):
+
+        if self.status == 'off':
+
+            self.status = 'on'
+
+        else:
+            self.status = 'off'
+
+
+if __name__ == "__main__":
+
+    id = int(input('Digite o id do dispositivo: '))
+    thermostat = Thermostat(id)
